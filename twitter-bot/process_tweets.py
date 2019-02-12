f391f2778caced97316cee12bfb2caeb21234f26
Eduardo Garcia <eduardo.garcia@esailors.de>
2019-02-12 12:57:16 +0000

    (bot) Add category badges to entries, update profile summary

--- twitter-bot/process_tweets.py
+++ twitter-bot/process_tweets.py
@@ -62,12 +62,17 @@ def process_tweet(tweet):
         data['activities'].insert(0, tweet_info)
         data['summary']['total_tacos'] += tweet_info['total_tacos']
 
-        # Add variety badges to entries
+        # Add category and variety badges to entries
         for entry in tweet_info['entries']:
+            if not entry['category'] in data['summary']['categories']:
+                entry['badges'].append('category')
+                data['summary']['categories'].append(entry['category'])
+
             if not entry['variety_key'] in data['summary']['varieties']:
                 entry['badges'].append('variety')
                 data['summary']['varieties'].append(entry['variety_key'])
 
+        data['summary']['total_categories'] = len(data['summary']['categories'])
         data['summary']['total_varieties'] = len(data['summary']['varieties'])
 
         dump_user_data(tweet.user.id_str, data)
@@ -127,6 +132,7 @@ def get_entry(variety_item):
     return {
         'amount': variety_amount,
         'badges': [],
+        'category': variety['category'],
         'text': entry_format.format(amount = variety_amount, variety = variety['name']),
         'variety_key': variety['key'],
     }
