a70d71486856df351965f224823785a0b8b40418
Eduardo Garcia <eduardo.garcia@esailors.de>
2019-03-28 22:36:25 +0000

    (bot) Update last processed id

--- twitter-bot/process_daily_activity.py
+++ twitter-bot/process_daily_activity.py
@@ -17,7 +17,7 @@ dry_run = False
 demo_summary_file = './assets/tacokeeper-summary.png'
 query = '#tacokeeper -filter:retweets'
 welcome_message = '¡Hola, @{screen_name}! Tu perfil estará disponible pronto en https://tacokeeper.com/{screen_name}'
-last_processed_id = '1110975260994797568'
+last_processed_id = '1111301268994228225'
 tk_url_profile_prefix = 'https://tacokeeper.com/{screen_name}?t='
 tk_url_prefix = 'https://tacokeeper.com/?t='
 varieties = []
