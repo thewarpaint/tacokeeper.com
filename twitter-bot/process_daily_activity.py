013256291828245b363a1dccda397ab6b34bc495
Eduardo Garcia <eduardo.garcia@esailors.de>
2019-04-30 17:09:04 +0000

    (bot) Add emoji to welcome message so URL is visible in tweet

--- twitter-bot/process_daily_activity.py
+++ twitter-bot/process_daily_activity.py
@@ -17,7 +17,7 @@ dry_run = False
 # TODO: Remove as many global variables as possible
 demo_summary_file = './assets/tacokeeper-summary.png'
 query = '#tacokeeper -filter:retweets'
-welcome_message = '¡Hola, @{screen_name}! Tu perfil estará disponible pronto en https://tacokeeper.com/{screen_name}'
+welcome_message = '¡Hola, @{screen_name}! Tu perfil estará disponible pronto en https://tacokeeper.com/{screen_name} 🎉'
 last_processed_id = '1123031840619618304'
 tk_url_profile_prefix = 'https://tacokeeper.com/{screen_name}?t='
 tk_url_prefix = 'https://tacokeeper.com/?t='
