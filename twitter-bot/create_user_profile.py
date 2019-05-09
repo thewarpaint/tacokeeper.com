2ad804b774675ca3a68729af6c113422d6aeb777
Eduardo Garcia <eduardo.garcia@esailors.de>
2019-05-09 03:18:26 +0000

    (bot) Extract download user profile to Tweepy helper

--- twitter-bot/create_user_profile.py
+++ twitter-bot/create_user_profile.py
@@ -4,14 +4,10 @@
 from datetime import date
 import locale
 import sys
-import urllib
 
-from tweepy_helper import get_api, get_user_info, get_user_profile_image_url
+from tweepy_helper import (download_user_profile_image, get_api, get_user_info, get_user_profile_image_url,
+                           user_profile_absolute_image_path)
 from yaml_helper import dump_user_data
-
-user_profile_image_path = 'assets/images/twitter/{screen_name}.jpg'
-user_profile_relative_image_path = '../' + user_profile_image_path
-user_profile_absolute_image_path = 'https://tacokeeper.com/' + user_profile_image_path
 
 user_profile_path = '../user_profiles/{screen_name}.html'
 user_profile_template = """---
@@ -67,10 +63,6 @@ def create_user_profile_page(user):
 
     with open(user_profile_path.format(screen_name = user['screen_name']), 'w') as file:
         file.write(user_profile_md)
-
-def download_user_profile_image(user, screen_name):
-    urllib.urlretrieve(get_user_profile_image_url(user),
-        user_profile_relative_image_path.format(screen_name = screen_name))
 
 
 if __name__ == '__main__':
--- twitter-bot/tweepy_helper.py
+++ twitter-bot/tweepy_helper.py
@@ -2,6 +2,11 @@
 
 import os
 import tweepy
+import urllib
+
+user_profile_image_path = 'assets/images/twitter/{screen_name}.jpg'
+user_profile_relative_image_path = '../' + user_profile_image_path
+user_profile_absolute_image_path = 'https://tacokeeper.com/' + user_profile_image_path
 
 def get_api():
     auth = tweepy.OAuthHandler(os.environ['TKB_CONSUMER_KEY'], os.environ['TKB_CONSUMER_SECRET'])
@@ -19,3 +24,7 @@ def get_user_info(user):
 
 def get_user_profile_image_url(user):
     return user.profile_image_url_https.replace('_normal', '')
+
+def download_user_profile_image(user, screen_name):
+    urllib.urlretrieve(get_user_profile_image_url(user),
+        user_profile_relative_image_path.format(screen_name = screen_name))
