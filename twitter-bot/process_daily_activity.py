42e1c6e1c7f99bb8d3ea3e11b495e0400fb78eb6
Eduardo Garcia <eduardogarcia.xyz+gh@gmail.com>
2019-06-20 12:51:58 -0500

    (bot) Update last processed id

--- twitter-bot/process_daily_activity.py
+++ twitter-bot/process_daily_activity.py
@@ -19,7 +19,7 @@ dry_run = False
 demo_summary_file = './assets/tacokeeper-summary.png'
 query = '#tacokeeper -filter:retweets'
 welcome_message = '¡Hola, @{screen_name}! Tu perfil estará disponible pronto en https://tacokeeper.com/{screen_name} 🎉'
-last_processed_id = '1140653327820173312'
+last_processed_id = '1141750906733703173'
 tk_url_profile_prefix = 'https://tacokeeper.com/{screen_name}?t='
 tk_url_prefix = 'https://tacokeeper.com/?t='
 varieties = []
