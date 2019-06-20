886dfd70d6c39e8c92ebd8ee4adfc0e0ba488df8
Eduardo Garcia <eduardogarcia.xyz+gh@gmail.com>
2019-06-20 09:25:04 -0500

    (match) Add matched varieties to tweet

--- taco-match/src/components/TweetLink.jsx
+++ taco-match/src/components/TweetLink.jsx
@@ -5,9 +5,25 @@ import {getMatchText} from '../helpers';
 const tacoKeeperUrl = 'https://tacokeeper.com';
 const twitterIntentUrl = 'https://twitter.com/intent/tweet';
 
+function getMatchedVarietiesText(varieties) {
+  const conjunctionText = ' y ';
+  const matchedVarieties = varieties
+    .filter(variety => variety.isMatch)
+    .map(variety => variety.name);
+
+  // Works for arrays with length 0 (empty string), 1 ('first'), or 2 ('first and second')
+  if (matchedVarieties.length < 3) {
+    return matchedVarieties.join(conjunctionText);
+  }
+
+  return matchedVarieties.slice(0, matchedVarieties.length - 1).join(', ') +
+    conjunctionText +
+    matchedVarieties.slice(-1);
+}
+
 function getTweetHref(varieties, username) {
-  const tweetText = getMatchText(varieties, username);
-  const tweetContent = `${tweetText} #tacomatch #tacokeeper ${tacoKeeperUrl}/${username}/taco-match`;
+  let tweetContent = `${getMatchText(varieties, username)} Coincidimos en: ${getMatchedVarietiesText(varieties)} `;
+  tweetContent += `#tacomatch #tacokeeper ${tacoKeeperUrl}/${username}/taco-match`;
 
   return `${twitterIntentUrl}?text=${encodeURIComponent(tweetContent)}`;
 }
