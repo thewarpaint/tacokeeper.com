94b3a6ca5aebef1f45f0a324d0094282a9efe31f
Eduardo Garcia <eduardogarcia.xyz+gh@gmail.com>
2019-06-19 23:42:32 -0500

    (match) Add styles for variants

--- taco-match/src/components/VarietyMatchList.jsx
+++ taco-match/src/components/VarietyMatchList.jsx
@@ -6,14 +6,25 @@ const VarietyMatchList = ({onChange, varieties}) => {
       {
         varieties.map(variety => {
           return (
-            <li key={variety.name}>
-              <label>
+            <li key={variety.name}
+                className="variety">
+              <label className="variety__label">
                 <input
                   type="checkbox"
+                  className="variety__input"
                   checked={variety.isMatch}
                   onChange={(event) => onChange(event.target.checked, variety)}
                 />
-                {variety.name}
+
+                <div class="variety__text">
+                  <div class="variety__name">
+                    {variety.name}
+                  </div>
+
+                  <div class="variety__category">
+                    {variety.category}
+                  </div>
+                </div>
               </label>
             </li>
           );
